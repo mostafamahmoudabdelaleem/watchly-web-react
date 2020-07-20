44dfcc645e551540c013f067831352efc4c07507
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-07-20 03:12:09 +0200

    first dev
    * Installed Bootstrap, Fontawesome
    * Mocked main pages
    * Created install prompt component

---
+++ src/pages/Series.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class series extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
