5ed1553809453542da149f2bd2048f11e60d96fd
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-07-20 08:18:08 +0200

    fixed pages naming

--- src/pages/Series.js
+++ src/pages/Series.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import Navbar from '../components/navbar'
+import '../css/Components.css';
+import '../css/Components-media-575.css';
+import '../css/Components-media-991.css';
 
-export default class series extends Component {
+export default class Series extends Component {
     render() {
         return (
             <div>
-                
+                <Navbar activeTab="Series"/>
+                <h3>Series</h3>
             </div>
         )
     }
