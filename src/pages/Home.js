3d77ed4a85fc6c4fd4a40e02ab191d7080555574
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-07-20 08:21:49 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import Navbar from '../components/navbar'
+import '../css/Components.css';
+import '../css/Components-media-575.css';
+import '../css/Components-media-991.css';
+
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <h3>Homepage</h3>
+                <Navbar activeTab="Home"/>
+                <h3>home</h3>
             </div>
         )
     }
