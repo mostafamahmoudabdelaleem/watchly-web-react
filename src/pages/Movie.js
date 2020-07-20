8b09294a48b19925a4901b7a5af4d8292510b85c
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-07-20 08:19:20 +0200

    changed to function component

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useParams } from "react-router-dom";  
+import Navbar from '../components/navbar'
+import '../css/Components.css';
+import '../css/Components-media-575.css';
+import '../css/Components-media-991.css';
 
-export default class Movie extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+export default function Movie() {
+    let { id } = useParams();
+    return (
+        <div>
+            <Navbar />
+            <h3>Movie name: {id}</h3>
+        </div>
+    )
 }
