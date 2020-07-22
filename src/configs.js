5ced90cd8f63bd3f8498544c6587bdf85234f136
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-07-22 03:48:01 +0200

    Update configs.js

--- src/configs.js
+++ src/configs.js
@@ -1,9 +1,9 @@
-
 export const CONFIGS = {
-    BACKEND_API_URL: process.env.BACKEND_API_URL || "https://watchly-api.herokuapp.com/api/v1",
+    BACKEND_API_URL: process.env.BACKEND_API_URL,
     ALL_SERIES_PATH: "/all_series",
     ALL_MOVIES_PATH: "",
     GET_SERIES_PATH: "",
     GET_MOVIE_PATH: "",
     GET_VIDEO_LINKS_PATH: "",
 }
+console.log(process.env);
