3d46a6f56edc4d8ed93ced7386970779d6b277b5
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-07-22 19:04:40 +0200

    Update displayItem.js

--- src/components/displayItem.js
+++ src/components/displayItem.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import RatingBadge from './ratingBadge'
 import QualityBadge from './qualityBadge'
 
 export default class DisplayItem extends Component {
+
+    generateLink = (link) => {
+        if(this.props.replace === "undefined" && this.props.with === "undefined"){
+            return link.split("https://eg4.akwam.net", 2)[1]
+        }else{
+            return (link.split("https://eg4.akwam.net", 2)[1]).replace(this.props.replace, this.props.with)
+        }
+    }
+
+
     render() {
         return (
             <React.Fragment>
                 <div className="col-6 col-sm-4 col-md-3 col-lg-2 display-list-item">
-                    <a 
+                    <Link 
                         className="card bg-light text-white display-list-card"
-                        href={this.props.item.link}>
+                        to={this.generateLink(this.props.item.link)}>
                         <img 
                             src={this.props.item.img_link} 
                             className="card-img display-list-img" 
@@ -19,7 +30,7 @@ export default class DisplayItem extends Component {
                             <QualityBadge quality={this.props.item.quality} />
                             <RatingBadge rating={this.props.item.rating} />
                         </div>
-                    </a>
+                    </Link>
                 </div>
             </React.Fragment>
         )
