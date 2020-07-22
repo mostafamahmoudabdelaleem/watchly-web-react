039bdf8802fc500966bc5bfd6f830e5e60a34d41
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-07-22 02:25:10 +0200

    implemented data loading functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,64 @@
 import React, { Component } from 'react'
+import { CONFIGS } from '../configs'
 import Navbar from '../components/navbar'
+import Jumbotron from '../components/jumbotron'
+import DisplayList from '../components/displayList'
+import Loader from '../components/loader'
 import '../css/Components.css';
 import '../css/Components-media-575.css';
 import '../css/Components-media-991.css';
 
-
 export default class Home extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            allSeries: null,
+            movies: null,
+            api_url: CONFIGS.BACKEND_API_URL + CONFIGS.ALL_SERIES_PATH,
+            loaderIsHidden: false
+        };
+    }
+    componentDidMount(){
+        this.fetchData();
+    }
+
+    fetchData = () => {
+        fetch("https://cors-anywhere.herokuapp.com/"+this.state.api_url)
+        .then(res => res.json())
+        .then(data => {
+            this.setState({
+                allSeries: data,
+                loaderIsHidden:true
+            })  
+        }).catch(err => console.log(err))
+    }
+
     render() {
         return (
-            <div>
-                <Navbar activeTab="Home"/>
-                <h3>home</h3>
-            </div>
+            <React.Fragment>
+                {
+                    this.state.loaderIsHidden
+                    ?
+                        <React.Fragment>
+                            <Navbar activeTab="Home"/>
+                            <Jumbotron />
+                            {this.state.allSeries || this.state.movies  ? 
+                                <div className="container">
+                                    <h2 className="fg-orange">
+                                        <i className="fas fa-film"></i> Recent:
+                                    </h2>
+                                    <div className="row">
+                                        <DisplayList list={this.state.allSeries} />
+                                    </div>
+                                </div>
+                            :
+                                <p className="text-center">No Series Found</p>
+                            }
+                        </React.Fragment>
+                    :
+                        <Loader />
+                }
+            </React.Fragment>
         )
     }
 }
