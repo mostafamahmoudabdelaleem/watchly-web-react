616a19804411be705a48cc81bbe24018766e69cd
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-07-23 03:03:03 +0200

    implemented Profile UI

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { CONFIGS } from '../configs'
 import Navbar from '../components/navbar'
 import '../css/Components.css';
 import '../css/Components-media-575.css';
@@ -6,10 +7,43 @@ import '../css/Components-media-991.css';
 
 export default class Profile extends Component {
     render() {
+        let u = {
+            username: localStorage.getItem(CONFIGS.LOCAL_USER_NAME_KEY),
+            profilePic: localStorage.getItem(CONFIGS.LOCAL_USER_PIC_KEY),
+            email: localStorage.getItem(CONFIGS.LOCAL_USER_EMAIL_KEY),
+            provider: localStorage.getItem(CONFIGS.LOCAL_USER_AUTH_PROVIDER_KEY),
+            uuid: localStorage.getItem(CONFIGS.LOCAL_UUID_KEY)
+        }
+
         return (
             <div>
                 <Navbar activeTab="Mostafa"/>
-                <h3>Profile</h3>
+                <div className="container">
+                    <div className="row mt-4">
+                        <div className="col-12 col-sm-10 offset-sm-1">
+                            <div className="card">
+                                <div className="card-header fg-orange">
+                                    <i className="fas fa-user"></i> {u.username}'s Profile Details:
+                                </div>
+                                <div className="card-body row">
+                                    <div className="col-12 col-md-4">
+                                        <img className="rounded img-fluid" src={u.profilePic} alt={u.username}/>
+                                    </div>
+                                    <div className="col-12 col-md-8">
+                                        <ul className="list-group list-group-flush">
+                                            <li className="list-group-item"><b>ID: </b>{u.uuid}</li>
+                                            <li className="list-group-item"><b>Name: </b>{u.username}</li>
+                                            <li className="list-group-item"><b>Email: </b>{u.email}</li>
+                                            <li className="list-group-item"><b>Auth Provider: </b>{u.provider}</li>
+                                        </ul>
+                                        <h6 className="text-muted text-center mt-4"><i className="fas fa-info"></i>  This is all the data we know about you.</h6>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
