b8f700fd7ff26f73fa12a9053eaf81a13d21a98a
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-07-27 17:55:57 +0200

    Update videoPlayer.js

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -10,13 +10,19 @@ export default class VideoPlayer extends Component {
             typeof(this.props.links["1080p"]) === "undefined"){
             window.history.back()
         }
+        let defaultSrc = this.props.links["480p"];
+        if(typeof(this.props.links["480p"]) === "undefined"){
+            defaultSrc = this.props.links["720p"]
+        }else if(typeof(this.props.links["480p"]) === "undefined" && typeof(this.props.links["720p"]) === "undefined"){
+            defaultSrc = this.props.links["1080p"]
+        }
         return (
             <div className="card col-12 col-md-10 offset-md-1 mt-4 embed-responsive embed-responsive-16by9">
                 <video 
                     id="video_player" 
                     className="embed-responsive-item" 
                     poster={this.props.poster} 
-                    src={this.props.links["480p"]}
+                    src={defaultSrc}
                     controls="show">
                     <source src={this.props.links["480p"]} type="video/mp4" label="480p" res="480" />
                     {
