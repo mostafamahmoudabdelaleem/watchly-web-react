a2b6ce4fa0a9bdf515d126d9897a06a7dc33e1a0
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-07-30 20:48:59 +0200

    fix footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
 
 export default class Footer extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            className: "container-fluid text-center footer"
+        }
+    }
+    
+    componentDidMount(){
+        let body = document.body,
+        html = document.documentElement;
+
+        let height = Math.max( body.scrollHeight, body.offsetHeight, 
+            html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+        if(height <= window.innerHeight){
+            this.setState({
+                className: "container-fluid text-center footer footer-sticky"
+            })
+        }
+    }
+
     render() {
+
         return (
-            <footer className={this.props.sticky === "true" ? "container-fluid text-center footer footer-sticky" : "container-fluid text-center footer"}>
+            <footer className={ this.state.className }>
                 <p>Copyright © 2020 Watchly - All Rights Reserved.</p>
             </footer>
         )
