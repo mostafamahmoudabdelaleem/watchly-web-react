7a3810d08c5b89da35798b2ee10bd164c57286f5
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-08-12 03:51:33 +0200

    added videojs

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
+import videojs from 'video.js'
+import 'video.js/dist/video-js.css'
+import '../css/video-js.css'
+
 
 export default class VideoPlayer extends Component {
     
-    render() {
+    componentDidMount(){
+        this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
+            console.log('onPlayerReady', this)
+        });
+    }
 
+    componentWillUnmount() {
+        if (this.player) {
+          this.player.dispose()
+        }
+      }
+
+    render() {
         if(
             typeof(this.props.links["480p"]) === "undefined" && 
             typeof(this.props.links["720p"]) === "undefined" &&
@@ -19,25 +34,27 @@ export default class VideoPlayer extends Component {
         return (
             <div className="card col-12 col-md-10 offset-md-1 mt-4 embed-responsive embed-responsive-16by9">
                 <video 
-                    id="video_player" 
-                    className="embed-responsive-item" 
+                    ref={ node => this.videoNode = node }
+                    className="video-js vjs-default-skin embed-responsive-item"
+                    controls={true} preload="metadata"
+                    responsive="true"
                     poster={this.props.poster} 
                     src={defaultSrc}
-                    controls="show">
+                    data-setup='{}'>
                     <source src={this.props.links["480p"]} type="video/mp4" label="480p" res="480" />
                     {
                         typeof(this.props.links["720p"]) === 'undefined'
                         ?
                             ''
                         :
-                            <source src="" type="video/mp4" label="720p" res="720" />
+                            <source src={this.props.links["720p"]} type="video/mp4" label="720p" res="720" />
                     }
                     {
                         typeof(this.props.links["1080p"]) === 'undefined'
                         ?
                             ''
                         :
-                            <source src="" type="video/mp4" label="1080p" res="1080" />
+                            <source src={this.props.links["1080p"]} type="video/mp4" label="1080p" res="1080" />
                     }					
                 </video>
             </div>
