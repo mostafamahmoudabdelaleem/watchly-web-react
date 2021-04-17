27a20e42209bf3be30b664fd8f72574989633913
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2021-04-18 00:35:50 +0200

    added new APK version

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -4,7 +4,7 @@ export default function Landing() {
     return (
         <div className="loader-container">
             <div className="loader">
-                <a className="download-btn el-4" download href="/app-release.apk" >Download APK for Android</a>
+                <a className="download-btn el-4" download href="/watchly-v1.2.apk" >Download APK for Android v1.2</a>
             </div>
         </div>
     )
